3d0a5369b46a45f389dab5100d90fc8abde88842
jenssjogren <32362742+jenssjogren@users.noreply.github.com>
2018-04-06 00:15:02 +0200

    Added schematic

--- pic/Testrigg.X/nbproject/Package-default.bash
+++ pic/Testrigg.X/nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/pic.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=pic.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=pic/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/Testrigg.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=Testrigg.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=testrigg.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/pic/bin
+makeDirectory ${TMPDIR}/testrigg.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/pic.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/testrigg.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/pic.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/testrigg.x.tar *
 checkReturnCode
 
 # Cleanup
