cd12eaf40ef73b6a4e594d96a1cd180210d5f9fb
jenssjogren <32362742+jenssjogren@users.noreply.github.com>
2018-04-06 00:51:08 +0200

    Added Android application

---
+++ application/Testrigg/app/src/main/java/com/example/jens/testrigg/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.jens.testrigg;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_ENABLE_BT = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+        }
+
+        if (!mBluetoothAdapter.isEnabled()) {
+
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+    }
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_ENABLE_BT:
+                // When the request to enable Bluetooth returns
+                if (resultCode == Activity.RESULT_OK) {
+                    // Bluetooth is now enabled, so set up a chat session
+
+                } else {
+                    // User did not enable Bluetooth or an error occurred
+                    Toast.makeText(this, "Kunde inte aktivera bluetooth", Toast.LENGTH_LONG).show();
+                }
+        }
+    }
+}
